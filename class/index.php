1322f815e3941eb45b37c3764c8fbebcdd87e805
Alex <pakshin.2018@mail.ru>
2022-04-20 04:50:38 +0700

    class sts

--- class/index.php
+++ class/index.php
@@ -1,16 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-class User {
-	public $name;
-	public $login;
-	private $pass;
-	public function show() {
-		echo '<h3> Имя: '.$this->name.'</h3>';
-		echo '<h3> Логин: '.$this->login.'</h3>';
-	}
-}
+require_once 'user.php';
+
 $user = new User();
-$user->name = 'Alex';
-$user->login = 'a';
+$user->set('Alex', 'a', '123');
+$user->SaveToSession();
 $user->show();
 ?>
