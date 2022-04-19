3fae63c1a50b2d03dd2031edf17845aa1c786b19
Alex <pakshin.2018@mail.ru>
2022-04-20 05:12:58 +0700

    sp_stff

--- classn/index.php
+++ classn/index.php
@@ -4,13 +4,25 @@ require_once 'student.php';
 require_once 'group.php';
 require_once 'dis.php';
 
+function LoadStudents($group, $path) {
+	$file = nl2br(file_get_contents($path));
+	$students = explode('<br />', $file);
+	foreach ($students as $s) {
+		$tmp = explode(';', $s);
+		$surname = $tmp[0];
+		$name = $tmp[1];
+		$patr = $tmp[2];
+		new Student($name,$surname,$patr,$group);
+	}
+}
+
 $g1 = new Group(1, 'И-01');
 $g2 = new Group(1, 'И-02');
 $g3 = new Group(1, 'И-03');
 
-$s1 = new Student('Винокуров','Михаил','Игоревич', $g3);
-$s2 = new Student('Бобровский','Константин','Михайлович', $g3);
-$s2 = new Student('Бравлстарс','Эль','Примо', $g3);
+$s1 = new Student('Пакшин','Алексаднр','Андреевич', $g3);
+$s2 = new Student('','','', $g3);
+$s2 = new Student('','','', $g3);
 
 $d1 = new Discipline(1, 'ППО');
 $d2 = new Discipline(2, 'Оператор ЭВМ');
@@ -21,5 +33,7 @@ $g3->AddDiscipline($d1);
 $g3->AddDiscipline($d2);
 $g3->AddDiscipline($d3);
 $g3->AddDiscipline($d4);
+
+LoadStudents($g3, 'i03.txt');
 $g3->display();
 ?>
