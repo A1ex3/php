a2f717d73ba289201dd716392d794cb98e20a88a
Alex <pakshin.2018@mail.ru>
2022-04-22 01:54:29 +0700

    94.2-6

--- mvc/project/config/routes.php
+++ mvc/project/config/routes.php
@@ -2,6 +2,9 @@
 	use \Core\Route;
 
 	return [
-		new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
+		new Route('/user/:id/', 'user', 'show'),
+		new Route('/user/:id/:key/', 'user', 'info'),
+		new Route('/user/all', 'user', 'all'),
+		new Route('/users/first/:n/', 'user', 'first')
 	];
 
